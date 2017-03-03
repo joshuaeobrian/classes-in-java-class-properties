09ebf3acade5ff271623de44bdc627b039fc4780
Josh O'Brian <joshuaeobrian@gmail.com>
2017-03-03 12:49:49 -0500

    first Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,26 @@ public class Main {
     public static void main(String[] args) {
 
         // todo: create a new variable "planet1" and set it to a new instance of the Planet class
-
+        Planet planet1 = new Planet();
         // todo: set all four properties of the Planet class on the planet1 instance: name, distanceFromSun, satellites, and supportsLife.
-
+        planet1.name = "Mercury";
+        planet1.distanceFromSun=0.4;
+        planet1.satellites=3;
+        planet1.supportsLife=false;
         // todo: create another new variable named "planet2" and set it to a new instance of the Planet class
-
+        Planet planet2 = new Planet();
         // todo: set all four properties of the Planet class on the planet2 instance
-
+        planet2.name="Earth";
+        planet2.distanceFromSun=3.4;
+        planet2.satellites=200;
+        planet2.supportsLife=false;
         // todo: Use println() to print a String description of planet1 using string concatenation
         /*
             Example output: "Name: Earth, Distance from Sun: 1 AU, Satellites: 1 moon(s), Supports Life: true"
+
          */
+        System.out.println("Name: "+planet1.name+", Distance from Sun: "+planet1.distanceFromSun+
+                " AU, Satellites: "+planet1.satellites+" moon(s), Supports Life: "+planet1.supportsLife);
 
         // todo: Use println() to print a String description of planet2, just like above
 
@@ -28,11 +37,12 @@ public class Main {
             changed. This change impacts only that one property on that one
             instance of the object.
          */
-
+        System.out.println("Name: "+planet2.getName()+", Distance from Sun: "+planet2.getDistanceFromSun()+
+                " AU, Satellites: "+planet2.getSatellites()+" moon(s), Supports Life: "+planet2.isSupportsLife());
         // todo: change a single property on your planet1 instance
-
+            planet1.satellites=26;
         // todo: change a different property on your planet2 instance
-
+            planet2.supportsLife=true;
         // todo: Use println() to print a String description of planet1 again.
         /*
             Note that the property you changed on planet1 is reflected in the
@@ -40,9 +50,12 @@ public class Main {
             shows that these two instances are distinct and separate from each
             other.
          */
+        System.out.println("Name: "+planet1.name+", Distance from Sun: "+planet1.distanceFromSun+
+                " AU, Satellites: "+planet1.satellites+" moon(s), Supports Life: "+planet1.supportsLife);
 
         // todo: Use println() to print a String description of planet2, again.  Note that the property you changed on planet2 is reflected in the output.
-
+        System.out.println("Name: "+planet2.name+", Distance from Sun: "+planet2.distanceFromSun+
+                " AU, Satellites: "+planet2.satellites+" moon(s), Supports Life: "+planet2.supportsLife);
     }
 
 }
--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -6,7 +6,7 @@
 public class Planet {
 
     // todo: create a property named "name" that can hold a planet's name. EG: mercury
-
+    String name;
     /*
         A planet's distance from the sun is measured in Astronomical Units (AU).
         One AU is the distance from the Sun to Earth.
@@ -14,9 +14,43 @@ public class Planet {
         EG: Mercury's distance from the sun is 0.4 AU.
      */
     // todo: create a property named "distanceFromSun" that holds a planet's distance from the sun in Astronomical Units.
-
+    double distanceFromSun;
     // todo: create a property "satellites" that holds the number of moons a planet has. EG: Mercury has 0 satellites
-
+    int satellites;
     // todo: create a property "supportsLife" that indicates whether or not a planet supports life. EG: Mercury does not support life.
+    boolean supportsLife;
 
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDistanceFromSun() {
+		return distanceFromSun;
+	}
+
+	public void setDistanceFromSun(Double distanceFromSun) {
+		this.distanceFromSun = distanceFromSun;
+	}
+
+	public int getSatellites() {
+		return satellites;
+	}
+
+	public void setSatellites(int satellites) {
+		this.satellites = satellites;
+	}
+
+	public boolean isSupportsLife() {
+		return supportsLife;
+	}
+
+	public void setSupportsLife(boolean supportsLife) {
+		this.supportsLife = supportsLife;
+	}
 }
